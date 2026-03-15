c03d5c202bc9552fd3b0eedab92b0baf32f7c8d5
Phillip <phillip.wu@infothinker.com>
2026-03-16 02:44:03 +0800

    fix: use proper Vue component for custom layout routing

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,16 +1,8 @@
 import DefaultTheme from "vitepress/theme";
-import { h } from "vue";
-import { useData } from "vitepress";
-import HomeLayout from "./HomeLayout.vue";
+import Layout from "./Layout.vue";
 import "./style.css";
 
 export default {
   extends: DefaultTheme,
-  Layout() {
-    const { frontmatter } = useData();
-    if (frontmatter.value.layout === "home-custom") {
-      return h(HomeLayout);
-    }
-    return h(DefaultTheme.Layout);
-  },
+  Layout,
 };
